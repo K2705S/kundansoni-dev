bcac8059978bcb46e94a864751722bd6e5fd1e9f
kundansoni-dev <68193772+kundansoni-dev@users.noreply.github.com>
2021-07-06 20:01:09 +0530

    100DaysOfCode #1

---
+++ 100-DAYS -OF-CODE-#1.py
@@ -0,0 +1,6 @@
+# AND now we are letting input in
+add_num =  int(input("Enter the first number to add :"))
+add_num_2 = int(input("Enter the second number to add :"))
+SUM = add_num + add_num_2
+print(f"Your answer of the sum is {SUM}")
+# Lets run it
